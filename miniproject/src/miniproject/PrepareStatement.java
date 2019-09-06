f7d8ebb78160cf31cbc57c359bdce974445f27b7
kshitija12 <46256476+kshitija12@users.noreply.github.com>
2019-09-06 15:48:09 +0530

    Add files via upload

---
+++ miniproject/src/miniproject/PrepareStatement.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniproject;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author admin
+ */
+class PrepareStatement {
+
+    void setString(int i, String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    ResultSet executeQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
